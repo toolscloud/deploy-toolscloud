7133ca40bc8496b48cc38636ef215a36046769ec
andretadeu <andre.tadeu.de.carvalho@gmail.com>
2014-12-02 20:12:55 -0200

    Run mysql and redmine images.

--- docker_container_creator.sh
+++ docker_container_creator.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "Teste!"
+sudo docker run --name=mysql -d \
+  -e 'DB_NAME=redmine_production' -e 'DB_USER=redmine' -e 'DB_PASS=!AdewhmOP@12' \
+  -v /opt/mysql/data:/var/lib/mysql \
+  sameersbn/mysql:latest
+
+sudo docker run --name=redmine -d -p 80:80 --link mysql:mysql \
+  -v /opt/redmine/data:/home/redmine/data \
+  sameersbn/redmine:2.6.0-1
