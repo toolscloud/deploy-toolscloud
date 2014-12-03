e6ac8dc4a7bcb19b54327bcf98ccea5e0a45f631
andretadeu <andre.tadeu.de.carvalho@gmail.com>
2014-12-03 20:45:24 -0200

    Add container data. Change container mysql to postgresql. Change images from sameersbn/* to toolscloud/*

--- docker_container_creator.sh
+++ docker_container_creator.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-sudo docker run --name=mysql -d \
+sudo docker run --name=data -d -v /applications toolscloud/data
+
+sudo docker run --name=postgresql -d \
   -e 'DB_NAME=redmine_production' -e 'DB_USER=redmine' -e 'DB_PASS=!AdewhmOP@12' \
   -v /opt/mysql/data:/var/lib/mysql \
-  sameersbn/mysql:latest
+  --volumes-from data \
+  toolscloud/postgresql:latest
 
-sudo docker run --name=redmine -d --link mysql:mysql \
-   -p 8081:80 -p 8444:443 \
+sudo docker run --name=redmine -d --link postgresql:postgresql \
+  -p 8081:80 -p 8444:443 \
   -v /opt/redmine/data:/home/redmine/data \
-  sameersbn/redmine:2.6.0-1
+  --volumes-from data \
+  toolscloud/redmine:latest
 
-sudo docker run --name='gitlab' -d \
+sudo docker run --name='gitlab' -d --link postgresql:postgresql \
   -e 'GITLAB_PORT=8082' -e 'GITLAB_SSH_PORT=10022' \
   -p 10022:22 -p 8082:80 -p 8445:443 \
+  --volumes-from data \
   -v /var/run/docker.sock:/run/docker.sock \
   -v $(which docker):/bin/docker \
-  sameersbn/gitlab:7.5.2
+  toolscloud/gitlab:latest
