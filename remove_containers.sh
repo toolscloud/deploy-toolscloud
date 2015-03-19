2eca6b78086f12b1d4db2a652a0149feb1b5c0f1
Bruno Souza <bruno@javaman.com.br>
2015-03-18 21:48:01 -0300

    adding option to remove "all" containers, including the data one.

--- remove_containers.sh
+++ remove_containers.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-sudo docker stop $(sudo docker ps -aq | grep -v $(sudo docker ps -aq --filter 'name=data'))
-sudo docker rm $(sudo docker ps -aq | grep -v $(sudo docker ps -aq --filter 'name=data'))
-sudo rm -rf /applications
+
+if [ "$1" == "all" ]; then
+	sudo docker stop $(sudo docker ps -aq )
+	sudo docker rm $(sudo docker ps -aq )
+        sudo rm -rf /applications
+else
+	sudo docker stop $(sudo docker ps -aq | grep -v $(sudo docker ps -aq --filter 'name=data'))
+	sudo docker rm $(sudo docker ps -aq | grep -v $(sudo docker ps -aq --filter 'name=data'))
+fi
+
